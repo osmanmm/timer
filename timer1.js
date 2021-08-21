ac2b8f7b35bddc2f01f8b15e65fe421c1cafac64
osmanmm <mohamed.osman@outlook.com>
2021-08-21 23:07:26 +0000

    added timer file

--- timer1.js
+++ timer1.js
@@ -1,11 +1,14 @@
-
-  function greet() {
-(process.stdout.write('\x07').slice(2));
+function greet() {
+(process.stdout.write('\x07'));
     }
-  let  myArgs = process.argv;
+  let  myArgs = process.argv.slice(2);
   myArgs.forEach(element => {
+if (!isNaN(element) && Math.sign(element) > 0){
+    
     setTimeout(greet, element*1000);
-console.log(element);
+    
+}
+
   });
   
  
